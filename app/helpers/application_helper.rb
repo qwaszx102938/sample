1ee269cc9e0cb2ae7ce96da09f8e1cad1802ffb7
william xiao <willy.xiao1989@gmail.com>
2013-07-22 22:56:29 +0800

    1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title="williams's"
+    if page_title.empty?
+       base_title+" title is empty"
+    else
+      base_title+" "+page_title
+    end
+  end
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+let(:base_title){"williams's" }
+  
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,8 +12,12 @@ describe "Static pages" do
     
     it "should have the right title" do
   visit '/static_pages/home'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  expect(page).to have_title("#{base_title}")
 end
+  it "should not have 'home' in the title" do
+  visit'/static_pages/home'
+  expect(page).not_to have_title("Home")
+  end
   end
   
   
@@ -24,7 +29,7 @@ end
    end
        it "should have the right title" do
   visit '/static_pages/help'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  expect(page).to have_title("#{base_title} Help")
  end
  end
  
@@ -35,7 +40,7 @@ end
    end
        it "should have the right title" do
   visit '/static_pages/about'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  expect(page).to have_title("#{base_title} About Us")
  end
  end
 end
